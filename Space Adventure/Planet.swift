ba906b8b6a92d55e8193ed3a39a3111071908c33
Philippos Slicher <philipposslicher@gmail.com>
2015-10-14 14:28:56 +0400

    All known errors removed. Usable

--- Space Adventure/Planet.swift
+++ Space Adventure/Planet.swift
@@ -8,3 +8,14 @@
 
 import Foundation
 
+class Planet {
+    
+    let name: String
+    let description: String
+    
+    init(name: String, description: String) {
+        self.name = name;
+        self.description = description
+    }
+    
+}
